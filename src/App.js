20488f1fe438553568be05bb5d9427dc29c9129d
seisen <jochim6.2@hotmail.fr>
2022-10-07 17:10:22 +0200

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,56 +1,16 @@
 
 import './App.css';
-import React, { useState } from 'react';
-import { Button } from 'react-bootstrap';
+import {Bouton1} from './bouton1';
 
 
 function App() {
-  const positions_y = [0,50,100];
-  const positions_x = [0,100,200];
-
-  const [x,setX] = useState('0');
-  const [y,setY] = useState('0');
   
-  const  getRandomInt = (max) => {
-    return Math.floor(Math.random() * max);
-  }
-
-  const handleClick = (e) => {
-    e.preventDefault();
-
-    let _x  =getRandomInt(3);
-    let _y = getRandomInt(3);
-
-    while (x == _x || y == _y) {
-      _x  =getRandomInt(3);
-      _y = getRandomInt(3);
-    }
-
-    setX(positions_x[_x])
-    setY(positions_y[_y])
-  }
 
   return (
     <div className="App">
-  
-        
-          <Button 
-          className='button-1'
-          onMouseDown={handleClick} 
-          style ={{
-            top:y,
-            left:x,
-            transition:'all 0.05s ease-out'
-          }}
-          >
-        Try
-        </Button>
-     
-      
+          <Bouton1/>
     </div>
   );
 }
 
 export default App;
-//onClick={setX(positions_x[getRandomInt(3)])}
-//setX(positions_x[getRandomInt(3)])
