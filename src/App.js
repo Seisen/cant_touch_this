ec787ac23230906873e3abeb0ddbb3fdc116f3b7
seisen <jochim6.2@hotmail.fr>
2022-10-08 09:33:12 +0200

    2eme bouton + css fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import {Bouton1} from './bouton1';
+import {Bouton2} from './bouton2';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
           <Bouton1/>
+          <Bouton2/>
     </div>
   );
 }
---
+++ src/bouton2.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Button } from 'react-bootstrap';
+
+export function Bouton2 (){
+  const positions_y = [0,50,100];
+  const positions_x = [0,100,200];
+
+  const [x,setX] = useState('0');
+  const [y,setY] = useState('0');
+  
+  const  getRandomInt = (max) => {
+      return Math.floor(Math.random() * max);
+  }
+
+  const handleClick = (e) => {
+      e.preventDefault();
+
+      let _x  =getRandomInt(3);
+      let _y = getRandomInt(3);
+
+      while (x == _x || y == _y) {
+      _x  =getRandomInt(3);
+      _y = getRandomInt(3);
+      }
+
+      setX(positions_x[_x])
+      setY(positions_y[_y])
+  }
+    return (
+        <div className="conteneur1">
+          <div
+          className='hitbox2'
+           onMouseEnter={handleClick}
+           onMouseOver={handleClick}
+           style ={{
+             top:y,
+             left:x,
+             transition:'all 0.05s ease-out'
+             }}
+          >
+            <Button className='button-2'>
+                  Try
+              </Button>
+          </div>
+            
+             
+              
+         
+          
+        </div>
+      );
+};
